b3a1412bbcb18439fa1ca1febe8f750f1d4b5d65
richaagr <richa.agrawal89@gmail.com>
2016-01-10 01:19:51 +0530

    Text changer Async task

--- app/src/main/java/android/mobile/com/androidtrainings/MainActivity.java
+++ app/src/main/java/android/mobile/com/androidtrainings/MainActivity.java
@@ -2,12 +2,21 @@ package android.mobile.com.androidtrainings;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+  private TextChangerTask textChangerTask;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    textChangerTask = new TextChangerTask(this);
+    textChangerTask.execute("I am the changed text");
+  }
+
+  public void cancelTask(View view) {
+    textChangerTask.cancel(true);
   }
 }
--- app/src/main/java/android/mobile/com/androidtrainings/TextChangerTask.java
+++ app/src/main/java/android/mobile/com/androidtrainings/TextChangerTask.java
@@ -1,10 +1,49 @@
 package android.mobile.com.androidtrainings;
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
 
-public class TextChangerTask extends AsyncTask {
+public class TextChangerTask extends AsyncTask<String,Integer,String> {
+
+  private final Context context;
+
+  public TextChangerTask(Context context) {
+    this.context = context;
+  }
+
   @Override
-  protected Object doInBackground(Object[] params) {
-    return null;
+  protected void onPreExecute() {
+    super.onPreExecute();
+    Log.println(Log.INFO,"Pre Execute","Pre Execute");
+  }
+
+  @Override
+  protected String doInBackground(String... params) {
+      for (int i=0;i<10000000;i++){
+        publishProgress(i);
+      }
+    Log.println(Log.INFO, "doInBackgroundOver","***********doInBackgroundOver*********");
+    return params[0];
+  }
+
+  @Override
+  protected void onProgressUpdate(Integer... values) {
+    super.onProgressUpdate(values);
+    Log.println(Log.INFO, "onProgressMethodCall","Value is "+values.toString());
+  }
+
+  @Override
+  protected void onPostExecute(String string) {
+    Log.println(Log.INFO,"Post Execute","************************Post Execute******************");
+    MainActivity mainActivity = (MainActivity) this.context;
+    TextView view = (TextView)mainActivity.findViewById(R.id.random_text);
+    view.setText(string);
+  }
+
+  @Override
+  protected void onCancelled(String string) {
+    Log.println(Log.INFO,"Cancelled Executed","*********Cancelled Executed*******" + string);
   }
 }
