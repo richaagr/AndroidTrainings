4eb56271657552f54dfbfbf75f40416925c7acdf
richaagr <richa.agrawal89@gmail.com>
2016-01-09 16:10:15 +0530

    Initial commit with an empty activity

---
+++ app/src/main/java/android/mobile/com/androidtrainings/TextChangerTask.java
@@ -0,0 +1,10 @@
+package android.mobile.com.androidtrainings;
+
+import android.os.AsyncTask;
+
+public class TextChangerTask extends AsyncTask {
+  @Override
+  protected Object doInBackground(Object[] params) {
+    return null;
+  }
+}
